c2607042bbb936b473bbc32d32ded9d002a119d3
darya <dasha.andreyuk@gmail.com>
2024-10-10 11:52:43 +0200

    fix solution for Remove Duplicates task

--- src/easy/removeduplicates/RemoveDuplicates.java
+++ src/easy/removeduplicates/RemoveDuplicates.java
@@ -2,55 +2,37 @@ package easy.removeduplicates;
 
 public class RemoveDuplicates {
 
-	public static void main(String[] args) {
-		Integer[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
-		int[] expectedNums = { 0, 1, 2, 3, 4 };
+    public static void main(String[] args) {
+        int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+        int[] expectedNums = { 0, 1, 2, 3, 4 };
 
-		int k = removeDuplicates(nums);
+        int k = removeDuplicates(nums);
 
-		assert k == expectedNums.length;
-		for (int i = 0; i < k; i++) {
-			assert nums[i] == expectedNums[i];
-		}
-		printNums(nums, k);
-	}
+        assert k == expectedNums.length : "Length mismatch";
+        for (int i = 0; i < k; i++) {
+            assert nums[i] == expectedNums[i] : "Element mismatch at index " + i;
+        }
+        printNums(nums, k);
+    }
 
-	private static void printNums(Integer[] nums, int k) {
-		for (int i = 0; i < k; i++) {
-			System.out.print(nums[i] + " ");
-			;
-		}
-	}
+    private static void printNums(int[] nums, int k) {
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+    }
 
-	private static int removeDuplicates(Integer[] nums) {
-		int start = 0;
-		int finish = 0;
-		int numOfDuplicates = 0;
-		int sumNumOfDuplicates = 0;
-		for (int i = 1; i < nums.length - sumNumOfDuplicates; i++) {
-			if ((nums[i] != null) && (nums[start] == nums[i]) && i != nums.length - sumNumOfDuplicates - 1) {
-				finish += 1;
-			} else {
-				if (i == nums.length - sumNumOfDuplicates - 1)
-					numOfDuplicates = finish - start + 1;
-				else
-					numOfDuplicates = finish - start;
-				moveElements(nums, i, numOfDuplicates);
-				i = i - numOfDuplicates;
-				start = i;
-				finish = i;
-				sumNumOfDuplicates += numOfDuplicates;
-			}
-		}
-		return nums.length - sumNumOfDuplicates;
-	}
+    private static int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
 
-	private static void moveElements(Integer[] nums, int i, int numOfDuplicates) {
-		for (int j = i; j < nums.length; j++) {
-			nums[j - numOfDuplicates] = nums[j];
-		}
-		for (int j = 1; j <= numOfDuplicates; j++) {
-			nums[nums.length - j] = null;
-		}
-	}
+        int uniqueIndex = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[uniqueIndex++] = nums[i];
+            }
+        }
+        return uniqueIndex;
+    }
 }
