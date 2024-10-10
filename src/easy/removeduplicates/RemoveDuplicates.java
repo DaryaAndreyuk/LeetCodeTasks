e6008c7ba7004b11c6c5aff4666b81ee2e35d6bf
darya <dasha.andreyuk@gmail.com>
2024-10-10 11:27:35 +0200

    fix solution for Remove Duplicates task

--- src/easy/removeduplicates/RemoveDuplicates.java
+++ src/easy/removeduplicates/RemoveDuplicates.java
@@ -3,33 +3,54 @@ package easy.removeduplicates;
 public class RemoveDuplicates {
 
 	public static void main(String[] args) {
-		int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }; // Input array
-		int[] expectedNums = { 0, 1, 2, 3, 4 }; // The expected answer with correct length
+		Integer[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+		int[] expectedNums = { 0, 1, 2, 3, 4 };
 
-		int k = removeDuplicates(nums); // Calls your implementation
+		int k = removeDuplicates(nums);
 
 		assert k == expectedNums.length;
 		for (int i = 0; i < k; i++) {
 			assert nums[i] == expectedNums[i];
 		}
+		printNums(nums, k);
 	}
 
-	private static int removeDuplicates(int[] nums) {
-		int[] output = new int[nums.length];
+	private static void printNums(Integer[] nums, int k) {
+		for (int i = 0; i < k; i++) {
+			System.out.print(nums[i] + " ");
+			;
+		}
+	}
+
+	private static int removeDuplicates(Integer[] nums) {
 		int start = 0;
 		int finish = 0;
-		int currentIndex = 0;
-		for (int i = 1; i < nums.length; i++) {
-			if (nums[start] == nums[i]) {
+		int numOfDuplicates = 0;
+		int sumNumOfDuplicates = 0;
+		for (int i = 1; i < nums.length - sumNumOfDuplicates; i++) {
+			if ((nums[i] != null) && (nums[start] == nums[i]) && i != nums.length - sumNumOfDuplicates - 1) {
 				finish += 1;
 			} else {
+				if (i == nums.length - sumNumOfDuplicates - 1)
+					numOfDuplicates = finish - start + 1;
+				else
+					numOfDuplicates = finish - start;
+				moveElements(nums, i, numOfDuplicates);
+				i = i - numOfDuplicates;
 				start = i;
 				finish = i;
-				output[currentIndex] = nums[i - 1];
-				currentIndex += 1;
+				sumNumOfDuplicates += numOfDuplicates;
 			}
 		}
-		return output.length;
+		return nums.length - sumNumOfDuplicates;
 	}
 
+	private static void moveElements(Integer[] nums, int i, int numOfDuplicates) {
+		for (int j = i; j < nums.length; j++) {
+			nums[j - numOfDuplicates] = nums[j];
+		}
+		for (int j = 1; j <= numOfDuplicates; j++) {
+			nums[nums.length - j] = null;
+		}
+	}
 }
