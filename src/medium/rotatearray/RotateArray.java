3754a2ad253f7e2c8cb2ae4f31b8bfb46ceb1e00
darya <dasha.andreyuk@gmail.com>
2024-10-15 13:34:49 +0200

    add solutions

--- src/medium/rotatearray/RotateArray.java
+++ src/medium/rotatearray/RotateArray.java
@@ -9,14 +9,14 @@ public class RotateArray {
 		// 7 6 5 4 3 2 1
 		// output: [5,6,7,1,2,3,4]
 
-		int[] output = rotateArray(nums, k);
-		printArray(output);
+		rotate(nums, k);
+		printArray(nums);
 
 		int[] nums1 = { 1, 2, 3 };
 		int k1 = 1;
 
-		int[] output1 = rotateArray(nums1, k1);
-		printArray(output1);
+		rotate(nums1, k1);
+		printArray(nums1);
 
 	}
 
@@ -26,35 +26,22 @@ public class RotateArray {
 		System.out.println();
 	}
 
-	private static int[] rotateArray(int[] nums, int k) {
-	    int length = nums.length;
-	    k = k % length; // Adjust k if greater than length
-	    if (k == 0) {
-	        return nums; // No rotation needed
-	    }
+	public static void rotate(int[] nums, int k) {
+		int n = nums.length;
+		k = k % n;
+		reverse(nums, 0, n - k - 1);
+		reverse(nums, n - k, n - 1);
+		reverse(nums, 0, n - 1);
+	}
 
-	    // Reverse the whole array
-	    for (int i = 0; i < length / 2; i++) {
-	        int temp = nums[i];
-	        nums[i] = nums[length - i - 1];
-	        nums[length - i - 1] = temp;
-	    }
-
-	    // Reverse the first k elements
-	    for (int i = 0; i < k / 2; i++) {
-	        int temp = nums[i];
-	        nums[i] = nums[k - i - 1];
-	        nums[k - i - 1] = temp;
-	    }
-
-	    // Reverse the remaining elements from k to the end
-	    for (int i = k; i < (length + k) / 2; i++) {
-	        int temp = nums[i];
-	        nums[i] = nums[length - (i - k) - 1];
-	        nums[length - (i - k) - 1] = temp;
-	    }
-
-	    return nums;
+	public static void reverse(int[] arr, int i, int j) {
+		while (i <= j) {
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+			i++;
+			j--;
+		}
 	}
 
 }
