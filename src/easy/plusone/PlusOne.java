76041a2bffaff397a6aabf3b2b998071b3d875af
darya <dasha.andreyuk@gmail.com>
2024-10-22 14:14:38 +0200

    modify solution for 'plus one' task

--- src/easy/plusone/PlusOne.java
+++ src/easy/plusone/PlusOne.java
@@ -3,7 +3,7 @@ package easy.plusone;
 public class PlusOne {
 
 	public static void main(String[] args) {
-		int[] digits = { 0 };
+		int[] digits = {4,3,2,9 };
 		int[] output = { 4, 3, 3, 0 };
 
 		int[] output_actual = plusOne(digits);
@@ -19,23 +19,18 @@ public class PlusOne {
 	}
 
 	private static int[] plusOne(int[] digits) {
-		for (int i = digits.length - 1; i >= 0; i--) {
-			if (digits[i] < 9) {
-				digits[i]++;
-				break;
-			} else if (i != 0) {
-				digits[i] = 0;
-			} else if (i == 0) {
-				int[] output = new int [digits.length + 1];
-				if (digits[i] < 9)
-					output[0]  = digits[i]+1;
-				else 
-					output[0] = 1;
-				return output;
-			}
-
+		for (int i = digits.length - 1; i >= 0; i--) { 
+		    if (digits[i] < 9) {
+		        digits[i]++;
+		        return digits;  // Сразу возвращаем результат, если нет переноса
+		    }
+		    digits[i] = 0;  // Если цифра была 9, то ставим 0 и идем к старшему разряду
 		}
-		return digits;
+
+		// Если мы дошли до конца массива (например, для 999), создаем новый массив
+		int[] output = new int[digits.length + 1];
+		output[0] = 1;
+		return output;
 	}
 
 }
