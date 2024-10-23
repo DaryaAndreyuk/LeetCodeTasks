3084365cb2cd16408bdc787ea6f29175bd3eb7b0
darya <dasha.andreyuk@gmail.com>
2024-10-23 11:11:29 +0200

    add Two Sum solution

---
+++ src/easy/twosum/TwoSum.java
@@ -0,0 +1,32 @@
+package easy.twosum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import basic.UtilClass;
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+		int[] nums = { 2, 7, 11, 15 };
+		int target = 9;
+		int[] expectedOutput = { 0, 1 };
+		int[] actualOutput = twoSum(nums, target);
+		UtilClass.printArray(actualOutput);
+	}
+
+	private static int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> twoSumMap = new HashMap<Integer, Integer>();
+		int [] output = new int[2];
+		for(int i = 0; i < nums.length; i++) {
+			if (!twoSumMap.containsValue(nums[i]) && !twoSumMap.containsKey(target - nums[i])) {
+				twoSumMap.put(nums[i], target - nums[i]);
+				output[0] = i;
+			} else {
+				output[1] = i;
+			}
+		}
+		return output;
+	}
+
+}
