a1325c8224ee253e3b3ce414561c970301baeda6
darya <dasha.andreyuk@gmail.com>
2024-10-25 11:52:02 +0200

    add Two Sum solution

--- src/easy/twosum/TwoSum.java
+++ src/easy/twosum/TwoSum.java
@@ -8,20 +8,27 @@ import basic.UtilClass;
 public class TwoSum {
 
 	public static void main(String[] args) {
-		int[] nums = { 2, 7, 11, 15 };
-		int target = 9;
-		int[] expectedOutput = { 0, 1 };
+		int[] nums = { 3,3 };
+		int target = 6;
+		// int[] expectedOutput = { 0, 1 };
 		int[] actualOutput = twoSum(nums, target);
 		UtilClass.printArray(actualOutput);
 	}
 
 	private static int[] twoSum(int[] nums, int target) {
-		Map<Integer, Integer> twoSumMap = new HashMap<Integer, Integer>();
-		int[] output = new int[2];
-		for (int i = 0; i < nums.length; i++) {
-			twoSumMap.put(nums[i], target - nums[i]);
-		}
-		return output;
+		Map<Integer, Integer> indexMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (indexMap.containsKey(complement)) {
+                return new int[] {indexMap.get(complement), i};
+            }
+            indexMap.put(nums[i], i);
+        }
+        
+        // Return an empty array or handle it according to your requirement if no solution exists
+        return new int[0];
+		
 	}
 
 }
